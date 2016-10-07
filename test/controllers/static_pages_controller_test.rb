4e3d40f368ad9e48fde64fd8fb8616171c77d052
Adam Sunshine <adam@csuac.com>
2016-10-06 19:23:28 -0600

    finished static pages layout

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,29 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+
+
+  test "should get root" do
+    get root_url
     assert_response :success
   end
+
+  # test "should get home" do
+  #   get static_pages_home_url
+  #   assert_response :success
+  #   assert_select "title", "Home | SmallWins Application by Adam Sunshine"
+  # end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | SmallWins Application by Adam Sunshine"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | SmallWins Application by Adam Sunshine"
   end
 
 end
